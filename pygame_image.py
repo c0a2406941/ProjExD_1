d0d1fea2150b486fb19388348d5a66d5ba7c1091
C0A24069 <c0a2406941@edu.teu.ac.jp>
2025-11-18 17:11:24 +0900

    練習9:はいけいるーぷ

--- pygame_image.py
+++ pygame_image.py
@@ -19,12 +19,13 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr
+        x = tmr % 3200
         screen.blit(bg_img, [-x, 0])#練習5
         screen.blit(bg_img2, [-x + 1600 ,0])#練習7
+        screen.blit(bg_img, [-x + 3200, 0])#練習5
         screen.blit(kk_img,[300, 200])#練習4
         pg.display.update()
-        tmr += 1        
+        tmr += 1
         clock.tick(200)#練習6
 
 
